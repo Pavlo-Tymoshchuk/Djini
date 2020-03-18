8720db8beb9d7de815c53eb032b22947f5aeb02d
Pavel-Tymoshchuk <38438426+Pavel-Tymoshchuk@users.noreply.github.com>
2020-03-18 19:00:39 +0200

    ++

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// Infinty slider
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+    let arrowsInfinity = document.querySelectorAll('.js-arrow-infinity');
+    
+    function initialInfinitySlider() {
+        for(var i = 0;arrowsInfinity.length > i; i++) {
+            let slider = arrowsInfinity[i].closest(".slider-infinity");
+            let arrowNext = arrowsInfinity[i].querySelector('.next');
+            let arrowPrev = arrowsInfinity[i].querySelector('.prev');
+            let sliderList = slider.querySelector('.js-infinity-slider-list');
+
+            
+            arrowNext.addEventListener('click', function() {
+                let itemShow = slider.querySelector('.js-slider-item-infinity.show');
+                itemShow.nextElementSibling.classList.add('show');
+                itemShow.classList.remove('show');
+                
+                setTimeout(function(){
+                    let newElem = itemShow;
+                    itemShow.remove();
+                    sliderList.append(newElem);
+                },750);
+                
+            });
+            
+            arrowPrev.addEventListener('click', function() {
+                let itemShow = slider.querySelector('.js-slider-item-infinity.show');
+                let lastElem = sliderList.lastElementChild;
+
+                sliderList.prepend(lastElem);
+                
+                setTimeout(function(){
+                    itemShow.previousElementSibling.classList.add('show');
+                    itemShow.classList.remove('show');
+                },20);
+            });
+            
+            var startPointX;
+            var startPointY;
+            slider.addEventListener("touchstart", function(event) {
+                startPointX = event.changedTouches[0].screenX;
+                startPointY = event.changedTouches[0].screenY;
+            }, false);
+            slider.addEventListener("touchend", function(event){
+                var endPointX = event.changedTouches[0].screenX;
+                var endPointY = event.changedTouches[0].screenY;
+                
+                if(startPointX - endPointX > 40) {
+                    arrowNext.click();
+                } else if(endPointX - startPointX > 40) {
+                    arrowPrev.click();
+                }
+            }, false);
+        }
+    }
+    
+    initialInfinitySlider();
+
+});
